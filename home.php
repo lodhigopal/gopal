002c7cc85242aaa108e7b7f94c5dc138dc254ea3
lodhigopal <51362826+lodhigopal@users.noreply.github.com>
2019-06-04 17:06:42 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,36 @@
+<? php
+include(incl/header.php);
+include(incl/contente.php);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Basic Project</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"></a>
+    </div>
+    <ul class="nav navbar-nav">
+       <li class="active"><a href="home.php">Home</a></li>
+      <li class="active"><a href="sign_up.php">Sign Up</a></li>
+      <li class="active"><a href="login.php">Login</a></li>
+      <li class="active"><a href="contect.php">Contect Us</a></li>
+    </ul>
+  </div>
+</nav>s
+
+</body>
+</html>
+
+<? php
+include(incl/footer.php) ?>
